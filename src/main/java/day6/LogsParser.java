c5ada9876ba63a383a77f7b67c9172b31b56732c
yahuhen <yahuhen@gmail.com>
2020-09-12 23:40:54 +0300

    day6 LogsParser refactored

--- src/main/java/day6/LogsParser.java
+++ src/main/java/day6/LogsParser.java
@@ -1,40 +1,41 @@
 package main.java.day6;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LogsParser {
 
     public static void main(String[] args) {
 
-        String toParse = "access_log.2020.09.07 212.168.101.5 granted\n" +
+        String toParse =
+                "access_log.2020.09.07 212.168.101.5 granted\n" +
                 "access_log.2020.09.07 212.168.101.6 granted\n" +
                 "access_log.2020.09.07 212.168.101.5 granted\n" +
                 "access_log.2020.09.07 212.168.101.6 denied";
 
-        String[] st = toParse.split("\\n");
+        Pattern pattern = Pattern.compile(" (\\d+.){3}\\d.+");
+        Matcher matcher = pattern.matcher(toParse);
 
-        String[][] second = new String[st.length][2];
-        for (int i = 0; i < st.length; i++) {
-            String[] med = st[i].split(" ");
-            second[i][0] = med[1];
-            second[i][1] = med[2];
+        String[][] tempArray = new String[toParse.split("\\n").length][2];
+
+        int counter = 0;
+        while (matcher.find()) {
+            tempArray[counter++] = matcher.group().trim().split(" ");
         }
 
-        Object[][] finalk = new Object[second.length][3];
+        Object[][] finalArray = new Object[tempArray.length][3];
 
         int position = 0;
         boolean next;
 
-        for (int i = 0; i < st.length; i++) {
+        for (String[] strings : tempArray) {
             next = false;
-            for (Object[] el : finalk) {
-                if (null != el[0] && ((String) el[0]).equalsIgnoreCase(second[i][0])) {
-                    if (second[i][1].equalsIgnoreCase("granted")) {
-                        int x = (int) el[1];
-                        x += 1;
-                        el[1] = x;
+            for (Object[] element : finalArray) {
+                if (null != element[0] && ((String) element[0]).equalsIgnoreCase(strings[0])) {
+                    if (strings[1].equalsIgnoreCase("granted")) {
+                        element[1] = (int) element[1] + 1;
                     } else {
-                        int x = (int) el[2];
-                        x += 1;
-                        el[2] = x;
+                        element[2] = (int) element[2] + 1;
                     }
                     next = true;
                 }
@@ -44,21 +45,18 @@ public class LogsParser {
                 continue;
             }
 
-            finalk[position][0] = second[i][0];
-            if (second[i][1].equalsIgnoreCase("granted")) {
-                finalk[position][1] = 1;
-                finalk[position][2] = 0;
+            finalArray[position][0] = strings[0];
+            if (strings[1].equalsIgnoreCase("granted")) {
+                finalArray[position][1] = 1;
+                finalArray[position++][2] = 0;
             } else {
-                finalk[position][1] = 0;
-                finalk[position][2] = 1;
+                finalArray[position][1] = 0;
+                finalArray[position++][2] = 1;
             }
-            position++;
         }
 
-
-        for (Object[] el : finalk) {
+        for (Object[] el : finalArray) {
             System.out.printf("ip %s: ok - %s, failed - %s%n", el[0], el[1], el[2]);
         }
-
     }
 }
